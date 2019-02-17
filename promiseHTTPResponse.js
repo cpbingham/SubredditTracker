dbab2313db2bc6768ce95a6cf854ec0b6a48757b
cpbingham <cbingham96@gmail.com>
2019-02-17 18:35:03 -0500

    Created promiseHTTPResponse to make HTTPS request to subreddit for data

---
+++ promiseHTTPResponse.js
@@ -0,0 +1,44 @@
+const https = require('https');
+
+/**
+  This function defines a promise that when resolved creates an HTTPS GET request
+  to the desired SubReddit to retrieve the first  25 posts in JSON format.
+
+  Promise is rejected if the server response is not 200 or if the JSON is not able
+  to be parsed.
+
+  @param url specifies url to make GET requet to.
+**/
+const promiseHTTPResponse = (url) => new Promise((resolve, reject) => {
+  https.get(url, (res) => {
+    // HTTP Response Status Code
+    const statusCode = res.statusCode;
+
+    // If the status code is not 200 (OK), reject promise
+    if(statusCode !== 200){
+      reject('Invalid Subreddit');
+    }
+
+    // Read data into string
+    let rawData = '';
+    res.on('data', (chunk) => {
+      rawData += chunk;
+    });
+
+    res.on('end', () => {
+      // Once you have the entire page as a string, try to parse to JSON
+      try {
+        const parsedData = JSON.parse(rawData);
+
+        // If successful, resolve promise with data in JSON format
+        resolve(parsedDate);
+
+      } catch (e) {
+        // If page isn't in JSON format, reject promise
+        reject('Data not in JSON format');
+      }
+    })
+  });
+}); //end promiseHTTPResponse
+
+module.exports = promiseHTTPResponse;
