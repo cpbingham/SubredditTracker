ab6220b6b80eef3aa0137a3ca76aa5dcb4203f12
cpbingham <cbingham96@gmail.com>
2019-02-18 14:06:54 -0500

    Finished main function, subredditTracker. All functionality working

--- subredditTracker.js
+++ subredditTracker.js
@@ -14,25 +14,43 @@ const url = 'https://www.reddit.com/r/' + subreddit + '.json';
 const date = fileDate();
 
 //Create fileName in which subreddit data will be stored
-const fileName = `${subreddit}_${date}.txt`;
+let fileName = `${subreddit}_${date}.txt`;
+fileName = fileName.replace('/', '_');
 
+// recentPosts will be instantiated to be a PostList containing previous fetched posts.
 let recentPosts;
 
+/**
+  This asynchronous function handles the main logic of the application.
+**/
 async function subredditTracker() {
   try {
+    // Fetch data from desired SubReddit and insert into PostList Object.
     const jsonData = await promiseHTTPResponse(url);
     const posts = new PostList(jsonData);
 
     let newPosts;
 
+    // If this is the first run of the function, add the entire list of posts to the file.
     if (!recentPosts) {
       recentPosts = new PostList(jsonData);
       newPosts = recentPosts.toString();
       await promiseWriteFile(fileName, newPosts);
     }
+
+    // Otherwise compare the fetched data with the existing data in the file and only add the new posts.
+    else {
+      newData = recentPosts.compare(posts);
+      recentPosts.addPosts(newData);
+      await promiseWriteFile(fileName, newData.toString());
+    }
+
+    // If any of the promises reject, catch and display error.
   } catch (error) {
     console.log(error);
   }
 }
 
+// Run the program once initially and then re-run to update every 15 minutes.
 subredditTracker();
+setInterval(subredditTracker, 90000);
